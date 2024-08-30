4dfdaac78b17a1e7db366b2841aed2b22c9b958c
amulet1989 <muletdelosreyes89@gmail.com>
2024-08-30 10:15:34 -0300

    caja6

--- preprocesar.py
+++ preprocesar.py
@@ -38,7 +38,7 @@ import numpy as np
 ##########################################
 
 #### Obtener imagen de referencia ####
-pts = np.array([[113, 157], [104, 260], [134, 262], [139, 155]], dtype="int32")
+pts = np.array([[104, 184], [101, 275], [130, 277], [129, 182]], dtype="int32")
 # Caja5 [[102, 172], [101, 269], [129, 270], [128, 170]]
 # Caja6 [[104, 184], [101, 275], [130, 277], [129, 182]]
 # Caja7 [[113, 157], [104, 260], [134, 262], [139, 155]]
@@ -46,6 +46,6 @@ mask = np.zeros((480, 640), dtype=np.uint8)
 cv2.fillConvexPoly(mask, pts, 255)
 directory_path = utils.seleccionar_directorio()  # "./videos_capturados/Image_ref"
 imag_prom = utils.get_im_avg(
-    directory_path, mask, name="image_ref_caja7"
+    directory_path, mask, name="image_ref_caja6"
 )  # Obtener imagen promedio
 ########################################
